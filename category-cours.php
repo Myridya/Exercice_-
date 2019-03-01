add669cf743a08f4dc88f3a642e11f872d8a2561
Carolanne <myridya@hotmail.com>
2019-03-01 18:36:36 -0500

    Commit 3: Ajustement de la grille avec les différentes médias

--- category-cours.php
+++ category-cours.php
@@ -24,7 +24,10 @@ get_header();
 				?>
 			</header><!-- .page-header -->
 
+			<section class="cours">
+
 			<?php
+			
 			// Start the Loop.
 			while ( have_posts() ) :
 				the_post();
@@ -39,13 +42,10 @@ get_header();
 				// End the loop.
 			endwhile;
 
-			// Previous/next page navigation.
-			twentynineteen_the_posts_navigation();
+			?>
+			</section>
 
-			// If no content, include the "No posts found" template.
-		else :
-			get_template_part( 'template-parts/content/content', 'none' );
-
+		<?php
 		endif;
 		?>
 		</main><!-- #main -->
